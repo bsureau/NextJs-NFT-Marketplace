50f2a325dd0465d4043b63a7b86a181961e0c4f5
Benjamin SUREAU <benjamin.sureau@evaneos.com>
2022-06-18 19:42:05 +0200

    feat: retrieve listed item on homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,29 @@
-import type { NextPage } from 'next'
-import styles from '../styles/Home.module.css'
+import type { NextPage } from "next"
+import styles from "../styles/Home.module.css"
+import { useMoralisQuery } from "react-moralis"
+import NFTBox, { NFTBoxProps } from "../components/NftBox"
 
 const Home: NextPage = () => {
-  return (
-    <div className={styles.container}>
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          HI
-        </h1>
-      </main>
-    </div>
-  )
+    const { data: listedNfts, isFetching: fetchingListedNfts } = useMoralisQuery(
+        "ActiveItem",
+        (query) => query.limit(10).descending("tokenId")
+    )
+
+    console.log(listedNfts)
+
+    return (
+        <div className={styles.container}>
+            {fetchingListedNfts ? (
+                <div>Loading...</div>
+            ) : (
+                listedNfts.map((nft /*, index*/) => {
+                    console.log(nft.attributes)
+
+                    return <div>WIP</div>
+                })
+            )}
+        </div>
+    )
 }
 
 export default Home
