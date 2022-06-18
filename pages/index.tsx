383bffc084c557493e90ea24072d44a2122a6f3d
Benjamin SUREAU <benjamin.sureau@evaneos.com>
2022-06-18 20:36:06 +0200

    refactor: index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,34 +1,54 @@
 import type { NextPage } from "next"
-import styles from "../styles/Home.module.css"
-import { useMoralisQuery } from "react-moralis"
-import NFTBox, { NFTBoxProps } from "../components/NftBox"
+import { useMoralis, useMoralisQuery } from "react-moralis"
+import { useEffect, useState } from "react"
+import NFTBox from "../components/NftBox"
+
+const PAGE_SIZE = 9
 
 const Home: NextPage = () => {
+    // TODO: Implement paging in UI
+    const [page, setPage] = useState(1)
+    const { isWeb3Enabled } = useMoralis()
+
     const { data: listedNfts, isFetching: fetchingListedNfts } = useMoralisQuery(
         "ActiveItem",
-        (query) => query.limit(10).descending("tokenId")
+        (query) =>
+            query
+                .limit(PAGE_SIZE)
+                .descending("tokenId")
+                .skip((page - 1) * PAGE_SIZE)
     )
 
-    console.log(listedNfts)
-
     return (
-        <div className={styles.container}>
-            {fetchingListedNfts ? (
-                <div>Loading...</div>
-            ) : (
-                listedNfts.map((nft /*, index*/) => {
-                    console.log(nft.attributes)
-                    const nftAttributes: NFTBoxProps = nft.attributes
-                    return (
-                        <NFTBox
-                            {...nftAttributes}
-                            key={`${nftAttributes.nftAddress}${nftAttributes.tokenId}`}
-                        />
+        <div className="container mx-auto">
+            <h1 className="py-4 px-4 font-bold text-2xl">Recently Listed</h1>
+            <div className="flex flex-wrap">
+                {isWeb3Enabled ? (
+                    fetchingListedNfts ? (
+                        <div>Loading...</div>
+                    ) : (
+                        listedNfts.map((nft /*, index*/) => {
+                            console.log(nft.attributes)
+                            const { price, nftAddress, tokenId, marketplaceAddress, seller } =
+                                nft.attributes
+
+                            return (
+                                <NFTBox
+                                    price={price}
+                                    nftAddress={nftAddress}
+                                    tokenId={tokenId}
+                                    marketplaceAddress={marketplaceAddress}
+                                    seller={seller}
+                                    key={`${nftAddress}${tokenId}`}
+                                />
+                            )
+                        })
                     )
-                })
-            )}
+                ) : (
+                    <div>Web3 Currently Not Enabled </div>
+                )}
+            </div>
         </div>
     )
 }
-
 export default Home
